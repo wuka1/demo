3d764cd220222668330368694888dbe469d6fa29
wuka1 <wka2010@163.com>
2024-06-29 21:51:53 +0800

    checkstyle

--- demo-dao/src/main/java/Main.java
+++ demo-dao/src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("this is a checkstyle test");
-        System.out.println("second");
+        System.out.println("third");
     }
 }
