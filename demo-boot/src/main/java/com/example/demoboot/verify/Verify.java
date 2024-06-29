c05d12ef787c85dc456a25d4bdff934cc5998b6c
wuka1 <wka2010@163.com>
2024-06-29 18:49:04 +0800

    init

---
+++ demo-boot/src/main/java/com/example/demoboot/verify/Verify.java
@@ -0,0 +1,10 @@
+package com.example.demoboot.verify;
+
+import java.util.Date;
+
+public class Verify {
+
+    public static void main(String[] args) {
+        System.out.println(new Date().getTime());
+    }
+}
