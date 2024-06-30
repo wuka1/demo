009610854752f3648bd32804d4f0cc304dd22ce2
wuka1 <wka2010@163.com>
2024-06-30 17:28:06 +0800

    第11次

--- demo-boot/src/main/java/com/example/demoboot/Main.java
+++
@@ -1,9 +0,0 @@
-package com.example.demoboot;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-        System.out.println("this is a test");
-    }
-}
--- demo-dao/src/main/java/Main.java
+++ demo-dao/src/main/java/Main.java
@@ -1,12 +1,11 @@
+/**
+ * checkstyle检查测试.
+ *
+ * @author wuka
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("this is a checkstyle test");
-        System.out.println("third");
-        System.out.println("第4次");
-        System.out.println("第5次");
-        System.out.println("第6次");
-        System.out.println("第7次");
-        System.out.println("第8次");
-        System.out.println("第9次");
+
     }
 }
