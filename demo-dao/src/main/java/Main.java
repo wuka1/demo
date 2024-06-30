cbf81f7cab5d77d89038f32a8e7e1efb7af07a3a
wuka1 <wka2010@163.com>
2024-06-30 13:37:49 +0800

    第4次

--- demo-dao/src/main/java/Main.java
+++ demo-dao/src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("this is a checkstyle test");
         System.out.println("third");
+        System.out.println("第4次");
     }
 }
