cab4d685b1022c64df4d8f23411434628cc61cae
wuka1 <wka2010@163.com>
2024-07-01 11:46:18 +0800

    pmd1

---
+++ demo-boot/src/main/java/com/example/demoboot/Main.java
@@ -0,0 +1,9 @@
+package com.example.demoboot;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("this is a test");
+    }
+}
--- demo-dao/src/main/java/Main.java
+++ demo-dao/src/main/java/Main.java
@@ -5,5 +5,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("this is a checkstyle test");
+        System.out.println("第1次");
     }
 }
