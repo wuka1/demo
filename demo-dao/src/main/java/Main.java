95e02ed3dd9c0c4b276e10b4de86a8e19b3cc1e2
wuka1 <wka2010@163.com>
2024-07-01 15:17:40 +0800

    checkstyle

--- demo-dao/src/main/java/Main.java
+++ demo-dao/src/main/java/Main.java
@@ -1,10 +1,31 @@
-/**
- *
- */
+
 public class Main {
 
+
+    public Main() {
+
+    }
+    private String temp;
+    /**
+     *  打印方法.
+     *
+     * @param str 入参
+     *
+      */
+    public void print(String str) {
+//        System.out.println(str);
+    }
+
+    /**
+     *  main 方法.
+     *
+     * @param args str
+     */
     public static void main(String[] args) {
-        System.out.println("this is a checkstyle test");
-        System.out.println("第1次");
+        String str = "test";
+        Main m = new Main();
+//        System.out.println(str);
+        m.print(str);
+
     }
 }
