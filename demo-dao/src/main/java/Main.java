0ca95ce8e33725254efb3803410e04f5806b933a
wuka1 <wka2010@163.com>
2024-07-04 18:12:34 +0800

    t4

--- demo-dao/src/main/java/Main.java
+++ demo-dao/src/main/java/Main.java
@@ -1,4 +1,6 @@
-
+/**
+ *
+ */
 public class Main {
     private String temp1;
     private String temp2;
@@ -13,6 +15,8 @@ public class Main {
     public static void main(String[] args) {
         String str = "test";
         Main m = new Main();
+        String t1 = "test1";
+        String t2 = "test2";
         m.print(str);
     }
 }
